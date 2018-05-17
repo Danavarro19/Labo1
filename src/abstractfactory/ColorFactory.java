fee8a893dcffce80e3d9a20021ef792d1085f16f
Nelson Guardado <nguardadov@gmail.com>
2018-05-16 22:01:45 -0600

    CommitFactoria

---
+++ src/abstractfactory/ColorFactory.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package abstractfactory;
+
+import colors.Blue;
+import colors.Color;
+import colors.Green;
+import colors.Red;
+import shape.Shape;
+
+/**
+ *
+ * @author Nelson Guardado
+ */
+public class ColorFactory extends AbstractFactory{
+
+    @Override
+    Color getColor(String color) {
+        if(color == null){
+         return null;
+      }		
+      
+      if(color.equalsIgnoreCase("RED")){
+         return new Red();
+         
+      }else if(color.equalsIgnoreCase("GREEN")){
+         return new Green();
+         
+      }else if(color.equalsIgnoreCase("BLUE")){
+         return new Blue();
+      }
+      
+      return null;
+    }
+
+    @Override
+    Shape getShape(String shape) {
+        return null;
+    }
+    
+}
